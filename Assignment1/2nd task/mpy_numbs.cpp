c10188f3e2e194673d8b13260f06b4192c4eb59f
rizkanrdn <rizkanurdiana08@gmail.com>
2024-09-03 12:44:42 +0700

    Add files via upload

---
+++ Assignment1/2nd task/mpy_numbs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//variabel 
+	int n1 = 5;
+	int n2 = 10;
+	int hasil = 0;
+	
+	//perkalian
+	hasil = n1 * n2;
+	
+	//output
+	cout << hasil;
+	return 0;
+}
